35289db80aa53fb6bf15f314ed201aef63eb93f6
Hirano <hiranor@mail.gvsu.edu>
2015-10-16 18:40:02 -0400

    New BankGUI and two other things that I dont know

---
+++ src/BankApplication/BankGUI.java
@@ -0,0 +1,14 @@
+package BankApplication;
+
+import javax.swing.*;
+
+public class BankGUI extends JFrame{
+	private JButton add;
+	private JButton delete;
+	private JButton update;
+	private JButton clear;
+	
+	public BankGUI(){
+		
+	}
+}
