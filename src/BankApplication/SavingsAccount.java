9cd47a07bb2ecf164f745708306c60a16a20b249
Hirano <hiranor@mail.gvsu.edu>
2015-10-17 11:57:00 -0400

    Created Savings account. Dont know if its right or not but tried.

---
+++ src/BankApplication/SavingsAccount.java
@@ -0,0 +1,59 @@
+package BankApplication;
+
+import java.util.GregorianCalendar;
+
+public class SavingsAccount extends Account {
+
+	private static final long serialVersionUID = 1L;
+
+	private double minBalance;
+	private double interestRate;
+
+	public SavingsAccount(int num, String own, GregorianCalendar d,
+			double bal, double minBal, double rate) {
+		super(num, own, d, bal);
+		// TODO Auto-generated constructor stub
+		minBalance = minBal;
+		interestRate = rate;
+	}
+
+	public String toString(){
+		String result = super.toString();
+		result += "\nMinimum Balance: " + minBalance;
+		result += "\nInterest Rate: " + interestRate;
+
+		return result;
+	}
+
+	public boolean equals(Object other){
+		if (other instanceof Account){
+			SavingsAccount ant = (SavingsAccount) other;
+			if(super.equals(other) == true){
+				if (this.minBalance == ant.minBalance)
+					if(this.interestRate == ant.interestRate)
+						return true;
+			}
+		}
+		return false;
+	}
+
+	public double getMinBalance() {
+		return minBalance;
+	}
+
+	public void setMinBalance(double minBalance) {
+		this.minBalance = minBalance;
+	}
+
+	public double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+}
