0f6934788430423dabbc0eaed3390c35c34d261b
Hirano <hiranor@mail.gvsu.edu>
2015-10-16 20:47:58 -0400

    Made new changes and added a new main static class for display

--- src/BankApplication/BankGUI.java
+++ src/BankApplication/BankGUI.java
@@ -1,14 +1,42 @@
 package BankApplication;
 
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class BankGUI extends JFrame{
-	private JButton add;
+	private JButton addBTN;
 	private JButton delete;
 	private JButton update;
 	private JButton clear;
+	private JPanel btnPNL;
 	
 	public BankGUI(){
+		btnPNL = new JPanel(new GridLayout(4,1));
+		ButtonListener listener = new ButtonListener();
 		
+		addBTN = new JButton("Add");
+		addBTN.addActionListener(listener);
+		btnPNL.add(addBTN);
+		
+		delete = new JButton("Delete");
+		delete.addActionListener(listener);
+		btnPNL.add(delete);
+		
+		update = new JButton("Update");
+		update.addActionListener(listener);
+		btnPNL.add(update);
+		
+		clear = new JButton("Clear");
+		clear.addActionListener(listener);
+		btnPNL.add(clear);
+		
+		add(btnPNL);
+	}
+	
+	private class ButtonListener implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			
+		}
 	}
 }
